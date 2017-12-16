4adda43093a19ce2dc9ae7268a5429d82c556bc6
Peter Fischer <Peter.Fischer@dlr.de>
2017-12-16 20:58:49 +0100

    Image Processing Module.

---
+++ c++/image_proc/imageproc.cpp
@@ -0,0 +1,5 @@
+#include "imageproc.h"
+
+imageProc::imageProc()
+{
+}
---
+++ c++/image_proc/imageproc.h
@@ -0,0 +1,10 @@
+#ifndef IMAGEPROC_H
+#define IMAGEPROC_H
+
+class imageProc
+{
+public:
+    imageProc();
+};
+
+#endif // IMAGEPROC_H
